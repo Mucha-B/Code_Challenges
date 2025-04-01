a7f17d177302e4899d686e0d70869101bf71b3d5
Mucha-B <kierumucha@gmail.com>
2025-04-01 13:27:34 +0300

    SQL GROUP BY Question

--- 6_Coderbyte/1_Javascript/3 Find Intersection.js
+++ 6_Coderbyte/1_Javascript/3 Find Intersection.js
@@ -24,6 +24,10 @@ function FindIntersection(strArr){
     firstArr = firstArr.split(", ");
     secondArr = secondArr.split(", ");
 
+    // Below is same as above
+    // let firstArr = strArr[0].split(", ");
+    // let secondArr = strArr[1].split(", ");
+
     // firstArr = firstArr.map(Number);
     // secondArr = secondArr.map(Number);
 
@@ -40,11 +44,54 @@ function FindIntersection(strArr){
 
     return store;
 
-
 }
 
 
-let strArr = ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"];
-strArr = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+// Not my solution
+// Solution using includes()
+// The .includes() method checks if an array or string contains a specific value and returns true or false.
+function FindIntersectionUncludes(strArr) { 
 
-console.log(FindIntersection(strArr));
+    let newArr = [];
+    let elem1 = strArr[0].split(', ');
+    let elem2 = strArr[1].split(', ');
+    
+  
+    // code goes here  
+    for(let i = 0; i < elem1.length; i++ ) {
+      if (elem2.includes(elem1[i])) {
+        newArr.push(elem1[i]);
+      }
+    }
+
+    return newArr.toString();
+  
+  }
+
+
+
+
+//   Different application of foreach on array
+function FindIntersectionForEach(strArr) { 
+
+
+    let arrN1 = strArr[0].split(', ');
+    let arrN2 = strArr[1].split(', ');
+    let matchArr = [];
+
+    arrN2.forEach(function(e, i) {
+        if(arrN1.includes(e)){
+            matchArr.push(e);
+        }
+    });
+
+    // The .join() method is used to convert an array into a string, with elements separated by a specified delimiter.
+    // The join below converts the numbers gotten into a string and uses a comma as a separator/delimiter
+    return matchArr.length ? matchArr.join(',') : false;
+
+    
+    // [1, 2, 3, 4, 5]
+    // console.log(numbers.join("-"));  // Output: "1-2-3-4-5"
+    // console.log(numbers.join(", ")); // Output: "1, 2, 3, 4, 5"
+    // console.log(numbers.join(""));   // Output: "12345" (no separator)
+}
